80bbf4d441b3f300d037d354948713e00366bfef
Rafael Soutto <rafael@ice.dev.br>
2025-05-27 15:08:41 -0300

    add subnets output

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -7,3 +7,18 @@ output "vpc_cidr_block" {
   description = "The CIDR block of the VPC"
   value       = aws_vpc.custom_vpc.cidr_block
 }
+
+# output "subnet_ids" {
+#   description = "List of subnet IDs in the VPC"
+#   value       = aws_subnet.custom_subnet[*].id
+# }
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs in the VPC"
+  value       = aws_subnet.custom_public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs in the VPC"
+  value       = aws_subnet.custom_private[*].id
+}
